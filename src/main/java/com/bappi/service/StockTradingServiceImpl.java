7925be362e91e139b8bc7b099f53d300090e8b3d
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2025-11-19 19:38:29 +0600

    main: implment client streaming in the server

--- src/main/java/com/bappi/service/StockTradingServiceImpl.java
+++ src/main/java/com/bappi/service/StockTradingServiceImpl.java
@@ -1,9 +1,7 @@
 package com.bappi.service;
 
 import com.bappi.entity.Stock;
-import com.bappi.grpc.StockRequest;
-import com.bappi.grpc.StockResponse;
-import com.bappi.grpc.StockTradingServiceGrpc;
+import com.bappi.grpc.*;
 import com.bappi.repository.StockRepository;
 import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
@@ -53,6 +51,41 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
         } catch (Exception e) {
             responseObserver.onError(e);
         }
+    }
+
+    @Override
+    public StreamObserver<StockOrder> bulkStockOrder(StreamObserver<OrderSummary> responseObserver) {
+
+        return new StreamObserver<StockOrder>() {
+            private int totalOrders = 0;
+            private double totalAmount = 0;
+            private int successCount = 0;
+
+            @Override
+            public void onNext(StockOrder stockOrder) {
+                totalOrders++;
+                totalAmount += stockOrder.getPrice() * stockOrder.getQuantity();
+                successCount++;
+                System.out.println("Order #" + totalOrders + ": " + totalAmount + " " + successCount);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("Error: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                OrderSummary summary = OrderSummary.newBuilder()
+                                .setTotalOrders(totalOrders)
+                        .setTotalAmount(totalAmount)
+                                .setTotalAmount(totalAmount)
+                                        .build();
+                responseObserver.onNext(summary);
+                responseObserver.onCompleted();
+
+            }
+        };
     }
 
 }
